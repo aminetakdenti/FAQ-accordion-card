cf2faa780b9614b2d7cdcbcbfecb5a24fe0b111b
amine takdenti <71285134+aminetakdenti@users.noreply.github.com>
2022-07-19 19:21:58 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,45 @@
-let faqClick = document.querySelectorAll(".faq-info");
+let buttons = document.querySelectorAll("button");
+let prg = document.querySelectorAll("p");
 
-faqClick.forEach((ele) => {
-  ele.onclick = (e) => {
-    if (!ele.querySelector("button").classList.contains("hide")) {
-      e.target.parentElement.querySelector("p").style.fontWeight = "bold";
-      e.target.parentElement.querySelector("span").style.display = "block";
-      ele.querySelector("button").classList.toggle("hide");
+prg.forEach((p) => {
+  p.addEventListener("click", (e) => {
+    if (e.target.className == "hide") {
+      e.target.classList.remove("hide");
+      show(e);
     } else {
-      e.target.parentElement.querySelector("p").style.fontWeight = "normal";
-      e.target.parentElement.querySelector("span").style.display = "none";
-      ele.querySelector("button").classList.toggle("hide");
+      e.target.classList.add("hide");
+      hide(e);
     }
-  };
+  });
 });
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.className == "hide") {
+      e.target.classList.remove("hide");
+      show(e);
+    } else {
+      e.target.classList.add("hide");
+      hide(e);
+    }
+  });
+});
+
+const show = (evnts) => {
+  console.log(evnts.target.parentElement.querySelector("p"));
+  // Span Display Block
+  evnts.target.parentElement.querySelector("span").style.display = "block";
+  // Change The P Element fontWeight To Bold
+  evnts.target.parentElement.querySelector("p").style.fontWeight = "bold";
+  // Rotate The Btn To 180deg
+  evnts.target.style.transform = "rotate(180deg)";
+};
+const hide = (evnts) => {
+  console.log(evnts.target.parentElement.querySelector("p"));
+  // Span Display none
+  evnts.target.parentElement.querySelector("span").style.display = "none";
+  // Change The P Element fontWeight To Normale
+  evnts.target.parentElement.querySelector("p").style.fontWeight = "normale";
+  // Rotate The Btn To 0deg
+  evnts.target.style.transform = "rotate(0deg)";
+};
